72afa91513a4754602a9ec6531276a264e49688a
DESKTOP-1S608KI\WINDOWS <mittapabt2536@gmail.com>
2022-01-17 15:53:53 +0600

    sds

---
+++ ecome/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Homepage from './components/homepage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
